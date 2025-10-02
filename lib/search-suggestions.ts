1cd7aded4bbaa168067aad30ea77278e80fc28a8
Pranav-here <113384349+Pranav-here@users.noreply.github.com>
2025-10-01 23:19:12 -0500

    Create search-suggestions.ts

---
+++ lib/search-suggestions.ts
@@ -0,0 +1,7 @@
+export const famousPeople = [
+  { id: 'lebron', name: 'LeBron James', sport: 'Basketball' },
+  { id: 'ronaldo', name: 'Cristiano Ronaldo', sport: 'Soccer' },
+  { id: 'serena', name: 'Serena Williams', sport: 'Tennis' },
+  { id: 'messi', name: 'Lionel Messi', sport: 'Soccer' },
+  { id: 'neymar', name: 'Neymar Jr', sport: 'Soccer' },
+]
